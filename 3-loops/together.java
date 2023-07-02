482ac3e13fd1863983fbef9f3ce5040983f57f3c
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-02 16:12:13 +0530

    mssg

--- 3-loops/together.java
+++ 3-loops/together.java
@@ -1,9 +1,9 @@
-class together {
+class together{
     public static void main(String args[]) {
-        System.out.println("i j\n");
+        System.out.println("i's j's\n");
         for (int i = 1, j = 1; i <= 4 && j <= 7; i++, j++) {
             
-            System.out.println(i + " " + j);
+            System.out.println(i + "    " + j);
         }
     }
 }
