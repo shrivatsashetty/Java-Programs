729505e6e4f76afa3556c1527c922310379e64fe
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-02 13:29:01 +0530

    two hand in hand variables

---
+++ 3-loops/together.java
@@ -0,0 +1,9 @@
+class together {
+    public static void main(String args[]) {
+        System.out.println("i j\n");
+        for (int i = 1, j = 1; i <= 4 && j <= 7; i++, j++) {
+            
+            System.out.println(i + " " + j);
+        }
+    }
+}
