f966767f2361d405431f7e14435864e8c264f5d5
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-11 01:27:34 +0530

    git reborn

---
+++ 1-basics/userinput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class userinput
+{
+	public static void main(String[] args) {
+	    
+	    Scanner intake = new Scanner(System.in);
+	    System.out.print("Input an integer: ");
+	    int myint = intake.nextInt();
+	    System.out.println("Your int is " + myint);
+	    
+	    System.out.print("Enter a float: ");
+	    float myfloat = intake.nextFloat();
+	    System.out.println("Your float is " + myfloat);
+	    
+	    System.out.print("Enter a double: ");
+	    double mydouble = intake.nextDouble();
+	    System.out.println("Your double is " + mydouble);
+	    
+	    System.out.print("Enter a string: ");
+	    String mystring = intake.next();
+	    System.out.println("Your string is " + mystring);
+	    
+	    
+	    
+// 		System.out.println("");
+	}
+}
---
+++ 4-arrays/jarray1.java
@@ -0,0 +1,19 @@
+class jarray1{
+    public static void main(String[] args){
+        int[] numbers = {7,8,5,4,3,2};
+        System.out.println(numbers[0]);
+        float pointNums[]; // declaration of array
+        pointNums = new float[3];
+        // initialising one by one
+        pointNums[0] = 1.1f;
+        pointNums[1] = 2.4f;
+        pointNums[2] = 3.5F;
+        System.out.println(pointNums[0]);
+        System.out.println(numbers.length);
+        System.out.println("Printing array elements");
+        for(int i = 0; i < numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
