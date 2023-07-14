1723b82e784909a2c6d42e2a6c1414b65a604386
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-14 22:09:49 +0530

    overloading method

---
+++ 5-Methods/MethodOverloading1.java
@@ -0,0 +1,27 @@
+class MethodOverloading1{
+    // orignal method
+    public static int calculate(int number){
+        System.out.println("Method called with one argument, square is " + number * number);
+        return number * number;
+    }
+    //overloded
+    public static int calculate(int a, int b){
+        System.out.print("Method called with two argument, ");
+        System.out.println("Product is " + a * b);
+        return a * b;
+    } 
+    // overloaded
+     public static int calculate(double num1, double num2){
+        System.out.print("Method called with three argument, ");
+        System.out.println("Division yeilds " + num1 / num2);
+        return num1 / num2 ;
+    } 
+
+    public static void main(String[] args){
+        calculate(2);
+        calculate(2,4);
+
+    }
+
+
+}
