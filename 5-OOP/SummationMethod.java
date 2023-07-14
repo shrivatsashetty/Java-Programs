22161f7d6d6a5c1741293a63c05fdd4f1c1c9116
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-14 20:05:42 +0530

    methods in java

---
+++ 5-OOP/SummationMethod.java
@@ -0,0 +1,13 @@
+class SummationMethod{
+    // below we create a static method which is callable without an object
+    public static int calculateSum(int a, int b){ 
+        return a + b;
+    }
+    // in main method we call the static method
+    public static void main(String[] args){
+        int a = 2; int b = 5;
+        int result = calculateSum(a, b);
+        System.out.println("The sum is " + result);
+
+    }
+}
