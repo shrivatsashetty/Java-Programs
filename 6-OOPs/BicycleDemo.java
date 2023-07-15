b540c0898421bac2bf588ccecc1228b4df47c0d4
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-15 12:26:10 +0530

    even if classes are there in different files  but in same package program will run

--- 6-OOPs/BicycleDemo.java
+++ 6-OOPs/BicycleDemo.java
@@ -1,27 +1,4 @@
-class Bicycle{
-    int topGear = 5;
-    float topSpeed = 55.67f;
-    int currentGear = 0;
-    float currentSpeed = 23.45f;
 
-    void changeGear(){
-        this.currentGear++;
-    }
-    void slowDown(){
-        this.currentSpeed -= 3.5f;
-    }
-
-    void stop(){
-        this.currentSpeed = 0;
-    } 
-
-    void printStates(){
-        System.out.println("Current Gear: " + this.currentGear );
-        System.out.println("Current Speed: " + this.currentSpeed );
-
-    }
-
-}
 
 class BicycleDemo{
     public static void main(String[] args){
