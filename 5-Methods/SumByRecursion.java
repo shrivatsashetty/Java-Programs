90ab82ec76d817033f2fad0dc03ae5265b10e2c0
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-15 10:11:23 +0530

    summation using recursion

---
+++ 5-Methods/SumByRecursion.java
@@ -0,0 +1,16 @@
+class SumByRecursion{
+
+    protected static int summation(int n){
+        if(n <= 0){return 0;}
+        else{return n + summation(n-1); }
+        
+    }
+
+    public static void main(String[] args){
+        int number = 5;
+        int total = summation(number);
+        System.out.println("Sum of first " + number + " natural no's is " + total);
+    }
+
+    
+}
