ab0b764a0e14103149dce170ccbc383cee735921
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-15 12:44:53 +0530

    sample objct creation

---
+++ 6-OOPs/NumberAddDemo.java
@@ -0,0 +1,24 @@
+class NumberAdd {
+
+    static int a, b, c; // now other classes can also acess the variables, since they are static
+
+    static void getdata(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    static void add() {
+        c = a + b;
+        System.out.println("sum = " + c);
+    }
+}
+
+class NumberAddDemo {
+
+    public static void main(String[] args) {
+
+        NumberAdd obj = new NumberAdd();
+        obj.getdata(200, 100);
+        obj.add();
+    }
+}
