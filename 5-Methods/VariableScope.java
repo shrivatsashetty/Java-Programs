fb8b49185ab96261b2e91f07f6de03ddbb44e79f
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-15 09:33:04 +0530

    scope in java

--- 5-Methods/VariableScope.java
+++ 5-Methods/VariableScope.java
@@ -3,14 +3,14 @@ class VariableScope{
     private static int x = 20; // global variable within the class
     
     public static void main(String[] args) {
-        int y = 40; // scope limited to method 
-        System.out.println("Calling y from main method, y = " + y);
         System.out.println("Calling x from main method, x = " + x);
         myMethod();
     }
     
     private static void myMethod(){
         System.out.println("callin x from myMethod, x =  " + x);
+        int y = 40; // scope limited to method 
+        System.out.println("Calling y from myMethod, y = " + y);
     }
 
 }
