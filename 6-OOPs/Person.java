da4aa37964dbb5615b9b76cd352cd65cf94699e8
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-16 17:01:31 +0530

    constructors in java

---
+++ 6-OOPs/Person.java
@@ -0,0 +1,25 @@
+class Person{
+    private String name;
+    private int age;
+    
+    // below is the constructor method
+    public Person(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+
+    public void sayName(){
+        System.out.println("Hi my name is " + this.name + " and I'm " + this.age + " years old");
+    }
+
+
+    public static void main(String[] args){
+        Person person1 = new Person("Shashi", 30);
+        Person person2 = new Person("Manoj", 22); 
+        
+        person1.sayName();
+        person2.sayName();
+
+    }
+    
+ }
