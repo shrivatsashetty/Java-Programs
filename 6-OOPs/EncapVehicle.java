157abfd3acc82f5d16ab2d0ac36badd0464b6451
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-16 22:54:22 +0530

    Encapsulation using Vehicle as object

---
+++ 6-OOPs/EncapVehicle.java
@@ -0,0 +1,29 @@
+class Vehicle{
+    public String colour = "Red";
+    private String regdNo = "KA-20-57347";
+
+    public String getRegdNo(){
+        return regdNo;
+    }
+
+    public void setRegdNo(String newRegdNo){
+        this.regdNo = newRegdNo;
+    }
+}
+
+class EncapVehicle{
+    public static void main(String[] args){
+        Vehicle car = new Vehicle();
+        // System.out.println("Regd no: " + car.regdNo); // will give an acess error
+        String carRegdNo = car.getRegdNo();
+        System.out.println("calling getter");
+        System.out.println("Current regd no : " + carRegdNo);
+
+        System.out.println("After calling setter");
+        car.setRegdNo("MH-23-17243");
+        System.out.println("Current regd no : " + car.getRegdNo());
+
+
+
+    }
+}
