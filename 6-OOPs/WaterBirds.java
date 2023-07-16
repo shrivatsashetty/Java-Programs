739417b993be266923b67f8f863bf63b33b08913
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-17 02:02:33 +0530

    multi inheritence program in java

---
+++ 6-OOPs/WaterBirds.java
@@ -0,0 +1,24 @@
+interface Flyable{
+    public void fly();
+}
+
+interface Swimmer{
+    public void swim();
+}
+
+class WaterBirds implements Flyable, Swimmer{
+    public void fly(){
+        System.out.println("Flying.....");
+    }
+
+    public void swim(){
+        System.out.println("Swimming.....");
+    }
+    
+    public static void main(String[] args){
+        WaterBirds duck = new WaterBirds();
+        duck.swim();
+        duck.fly();
+    }
+}
+
