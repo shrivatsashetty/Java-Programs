55fd71740267e8fd9784f38c89e83c395b1955c6
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-17 01:21:33 +0530

    created a Square class

--- 7-Packages/Square.java
+++ 7-Packages/Square.java
@@ -1,3 +1,17 @@
 class Square{
-    
+    public double side;
+    public double diagonal;
+    private double area;
+
+    // constructor
+    Square(double side){
+        this.side = side;
+        this.diagonal = side * Math.sqrt(2);
+    }
+
+    double calcuateArea(double side){
+        this.area = side * Math.sqrt(2);
+        return this.area;
+    }
+
 }
---
+++ 7-Packages/SquareMain.java
@@ -0,0 +1,5 @@
+class SquareMain{
+    public static void main(String[] args){
+        
+    }
+}
