37487f0e1b52ce92a3f4e111a1c871056512d817
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-22 10:09:43 +0530

    new bike class with method overriding

---
+++ 6-OOPs/BikeMain.java
@@ -0,0 +1,33 @@
+
+ class Bike{
+    protected String company = "Bajaj";
+    public void throttle(){
+        System.out.println("Veee......Veeee..");
+    }
+ }
+
+class Pulsar extends Bike{
+    protected float engineCapacity;
+
+    Pulsar(float engineCC){
+        this.engineCapacity = engineCC;
+    }
+    public void horn(){
+        System.out.println("peeeep........side plzz.");
+    }
+    // overriding throttle method 
+    public void throttle(){
+        System.out.println("Vroooooom........");
+    }
+}
+
+class BikeMain{
+    public static void main(String[] args){
+        Pulsar pulsar200 = new Pulsar(200.0f);
+        System.out.println("bike Comapany: " + pulsar200.company);
+        System.out.println("Engine capacity: " + pulsar200.engineCapacity);
+        pulsar200.throttle();
+        pulsar200.horn();
+
+    }
+}
