cc47ad030f791fc3475e6a643768ab9e1a578ad2
shrivatsashetty <shrivatsa307@gmail.com>
2023-08-04 12:10:29 +0530

    corrected bugs

--- 2-conditinal_logical/logical.java
+++ 2-conditinal_logical/logical.java
@@ -5,8 +5,8 @@ class logical{
     public static void main(String args[]){
         int a = 51;
         int b = 26;
-        int x = 5;
-        int y = 4;
+        // int x = 5;
+        // int y = 4;
         boolean result = a >= 50 && b >= 25;
         System.out.println(result);
         System.out.println("opposite of true is " + !true); // false
