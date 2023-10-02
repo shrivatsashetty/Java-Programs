2b116630b5b8feeb2079a2273babeb3196f24b06
shrivatsashetty <shrivatsa307@gmail.com>
2023-10-02 14:22:38 +0530

    Made correction to class names and file names

--- 3-loops/JavaLoops.java
+++ 3-loops/JavaLoops.java
@@ -1,4 +1,4 @@
-class jLoops{
+class JavaLoops{
     public static void main(String args[]){
         int i = 0;
         int j = 0;
--- 3-loops/Together.java
+++ 3-loops/Together.java
@@ -1,4 +1,4 @@
-class together{
+class Together{
     public static void main(String args[]) {
         System.out.println("i's j's\n");
         for (int i = 1, j = 1; i <= 4 && j <= 7; i++, j++) {
