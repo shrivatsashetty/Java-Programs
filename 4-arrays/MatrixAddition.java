de6ab58ed821e5e737f17b48bab2bd8b2712177d
shrivatsashetty <shrivatsa307@gmail.com>
2024-01-26 18:38:24 +0530

    standardised

--- 4-arrays/MatrixAddition.java
+++ 4-arrays/MatrixAddition.java
@@ -1,13 +1,13 @@
 public class MatrixAddition {
     public static void main(String args[]){
         // lets consider two 3x3 matrices
-        int matrixA[][] = { {1, 4, 5}, {2, 6, 7}, {0, 3, 2} };
-        int matrixB[][] = { {4, 3, 2}, {1, 8, 0}, {2, 6, 3} };
+        int[][] matrixA = { {1, 4, 5}, {2, 6, 7}, {0, 3, 2} };
+        int[][] matrixB = { {4, 3, 2}, {1, 8, 0}, {2, 6, 3} };
 
         // we add the matrices only if the dimensions are equal
         if(matrixA.length == matrixB.length){
             // create a third matrix to store the result
-            int resultMatrix[][] = new int[ matrixA.length ][ matrixB.length ];
+            int[][] resultMatrix = new int[ matrixA.length ][ matrixB.length ];
 
             for(int i = 0; i < resultMatrix.length; i++){
                 for(int j = 0; j < resultMatrix.length; j++){
