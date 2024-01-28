9a3914d376527d55433fbc92c1ad3eb1a1a7d32b
shrivatsashetty <shrivatsa307@gmail.com>
2024-01-28 22:09:52 +0530

    added sufficient comments to undestand

--- 6-OOPs-II/AnonymousInnerClass.java
+++ 6-OOPs-II/AnonymousInnerClass.java
@@ -1,19 +1,20 @@
 /* In this program we create a simple class and an anonymous inner class */
-class Anonymous{
+abstract class Anonymous{
     // simple method
-    public void show(){
-        System.out.println("anonymous method");
-    }
+    public abstract void show();
 }
 
 public class AnonymousInnerClass {
     public static void main(String[] args){
 
-        // overriding one of the methods of the class during instantiation
+        /* overriding one of the methods of the class during instantiation
+            note that even though we canot create an object of abstract class
+            but here we are creating an object of anonymous inner class 
+        */ 
         Anonymous anonymous = new Anonymous(){
             @Override
             public void show(){
-                System.out.println("New implementation from anoymous class");
+                System.out.println("\nProviding implementation for abstract method by overriding");
             }
         };
 
