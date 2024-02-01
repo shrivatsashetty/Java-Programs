35b2138e5447b392bca395f264e74e1362947b0d
shrivatsashetty <shrivatsa307@gmail.com>
2024-02-01 20:25:26 +0530

    adding comments

--- 5-Methods/SumByRecursion.java
+++ 5-Methods/SumByRecursion.java
@@ -1,11 +1,13 @@
 class SumByRecursion{
 
+    // a recursive method outside defined outside the main method
     static int summation(int n){
         if(n <= 0){return 0;}
         else{return n + summation(n-1); }
         
     }
 
+    // main method calls the recursive method
     public static void main(String[] args){
         int number = 5;
         int total = summation(number);
