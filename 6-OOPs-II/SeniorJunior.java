94139404ebf1b97e6fbbb141a3567d3af9f631f8
shrivatsashetty <shrivatsa307@gmail.com>
2024-02-03 20:20:25 +0530

    resolved warnings

--- 6-OOPs-II/SeniorJunior.java
+++ 6-OOPs-II/SeniorJunior.java
@@ -3,6 +3,8 @@
 /* lets create a superclass called Senior, every java class extends Object class by default*/
 class Senior extends Object{
 
+    public int age; 
+
     // default constructor 
     public Senior(){
         System.out.println("Senior() Default");
@@ -11,6 +13,7 @@ class Senior extends Object{
     // parameterised constructor
     public Senior(int age){
         System.out.println("Senior() Parameterised");
+        this.age = age;
     }
 }
 
@@ -27,11 +30,13 @@ class Junior extends Senior{
     public Junior(int age){
         super(); // explicitly calling the default constructor of parent class, not required though
         System.out.println("Junior() Parameterised");
+        this.age = age;
     }
 
     public Junior(int age, String name){
         super(age); // to explicitly call the parameterised constructor of parent class 
         System.out.println("Junior() Multi-Parameterised");
+        this.age = age;
 
     }
 
@@ -44,10 +49,10 @@ public class SeniorJunior {
         System.out.println();
 
         Junior junior1 = new Junior(); // calls default constructor 
-        System.out.println();
+        System.out.println("Age:" + junior1.age + "\n");
         Junior junior2 = new Junior(5); // calls parameterised consructor
-        System.out.println();
+        System.out.println("Age:" + junior2.age + "\n");
         Junior junior3 = new Junior(10, "Naveen"); // calls multi param constructor 
-        System.out.println();
+        System.out.println("Age:" + junior3.age + "\n");
     }
 }
