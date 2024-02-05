22831a039c93180f75a92c6f070b08ed648ab9a5
shrivatsashetty <shrivatsa307@gmail.com>
2024-02-05 22:43:30 +0530

    anonymous inner class which implement interface

---
+++ 6-OOPs-I/InterWithAnonInnClass.java
@@ -0,0 +1,37 @@
+
+
+/* lets create a some interface */
+interface InterfaceA{
+    void show();
+    void display();
+}
+
+public class InterWithAnonInnClass {
+
+    public static void main(String[] args) {
+
+        /* we can't create an object of the interfce
+         * but we can create an object of the anonymous inner class of interface */
+        InterfaceA objA = new InterfaceA() {
+
+            // this anonymous inner class is now implementing the interface
+            // and hence must provide implementation to all the methods of the interface 
+
+            @Override
+            public void show(){
+                System.out.println("inside show method");
+            }
+            
+            @Override
+            public void display(){
+                System.out.println("inside display method");
+            }
+            
+        };
+
+        objA.show();
+        objA.display();
+
+    }
+
+}
